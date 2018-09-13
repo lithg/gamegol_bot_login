4830ae73d7de40e8563b031b86a26e1c9a2d9b6d
Guilherme Carvalho Lithg <35541154+lithg@users.noreply.github.com>
2018-09-13 00:37:49 -0300

    Added more security

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ driver = webdriver.Chrome('drivers\chromedriver.exe')
 
 driver.set_page_load_timeout(10)
 
+user = input("Digite o seu login: ")
+password = input("Digite sua senha: ")
+
 driver.get('http://google.com.br')
 driver.find_element_by_name('q').send_keys('Gamegol')
 driver.find_element_by_name('btnK').send_keys(Keys.ENTER)
 driver.find_element_by_partial_link_text('Game Gol').click()
-driver.find_element_by_name('iptUsuario').send_keys('user')
-driver.find_element_by_name('iptSenha').send_keys('password')
+driver.find_element_by_name('iptUsuario').send_keys(user)
+driver.find_element_by_name('iptSenha').send_keys(password)
 driver.find_element_by_name('iptRepass').click()
 driver.find_element_by_class_name('btOk').click()
 
